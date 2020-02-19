bf1031039a0799fa2f5d6bf7bbbe529197955dbd
Jeremy Cook <jeremycook123@gmail.com>
2020-02-20 10:48:24 +1300

    updated with checks for jq command before executing core script

--- install.cn.app.sh
+++ install.cn.app.sh
@@ -6,10 +6,12 @@
 # *yarn v1.17.3
 # *docker 19.03.5
 # *go 1.13.6
+# *jq 1.6
 
 command -v yarn >/dev/null 2>&1 || { echo >&2 "yarn command is required by this script - install and then rerun..."; exit 1; }
 command -v docker >/dev/null 2>&1 || { echo >&2 "docker command is required by this script - install and then rerun..."; exit 1; }
 command -v go >/dev/null 2>&1 || { echo >&2 "go command is required by this script - install and then rerun..."; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq command is required by this script - install and then rerun..."; exit 1; }
 
 echo here!
 exit 1
